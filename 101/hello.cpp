dc1dfd9552ec9945d9789ec04be12c5b0523dfe0
victor.zhang <zhangx.geo@yahoo.com>
2020-05-16 12:27:21 -0700

    first C++ program

---
+++ 101/hello.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Hello, world!" << endl;
+
+    
+    cout << "What is your name?" << endl;
+    cin>> s;
+    cout << "Hello, " << s << endl;
+    return 0;
+}
